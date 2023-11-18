0e06c475b5bb0dac3158f8056a0ea1b4ceeb20a6
baekseungchan1117 <bsc7386@gmail.com>
2023-11-18 11:15:20 +0900

    init

---
+++ src/main/java/com/example/crud11/Crud11Application.java
@@ -0,0 +1,13 @@
+package com.example.crud11;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Crud11Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Crud11Application.class, args);
+    }
+
+}
